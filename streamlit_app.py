dea180ba16839bc8250c5c700c972cafe533c163
hfemi <119875546+hfemi@users.noreply.github.com>
2022-12-06 16:46:28 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,11 +34,10 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # out put it the screen as table
 streamlit.dataframe(fruityvice_normalized)
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+ 
 
 
-# import snowflake.connector
+ import snowflake.connector
 
 # my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 # my_cur = my_cnx.cursor()
